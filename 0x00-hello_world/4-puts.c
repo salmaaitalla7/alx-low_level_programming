0c8ab6625945f7ba908bc53537bb789536c91950
salmaaitalla7 <salmaaitalla41@gmail.com>
2023-08-10 07:38:04 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Use puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
+
