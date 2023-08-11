43c23d68ee514cd62635c375e58c5904f9852473
salmaaitalla7 <salmaaitalla41@gmail.com>
2023-08-11 15:44:19 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * return: Always 0 (Success)
+ * main - Entry point i sometimes suffer from insomnia
+ * Return: Always 0 (Success)
  */
 
 int main(void)
