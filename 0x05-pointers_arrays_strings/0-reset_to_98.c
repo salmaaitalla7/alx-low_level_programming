a9fb9519a2bc87093c1b79d9374f562d6afba7d7
salmaaitalla7 <salmaaitalla41@gmail.com>
2023-08-21 15:48:30 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a poiter to an int and updates
+ * the value it points to 98
+ *
+ * @n: An integer input poiter
+ *
+ * Return : nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
